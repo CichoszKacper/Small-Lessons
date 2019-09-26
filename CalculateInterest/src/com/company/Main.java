f7e06c9eca6feb1721d7e2b60e14553291359a41
CichoszKacper <47371956+CichoszKacper@users.noreply.github.com>
2019-09-26 21:28:00 +0100

    Add files via upload

--- CalculateInterest/src/com/company/Main.java
+++ CalculateInterest/src/com/company/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         double i;
-        for (i=2; i<6;i++){
-            System.out.println("10000 at " + i + "% rate is " + calculateInterest(10000,i));
+        for (i=2; i<9;i++){
+            System.out.println("10000 at " + i + "% rate is " + String.format("%.2f",calculateInterest(10000,i)));
         }
     }
     public static double calculateInterest(double amount, double interestRate){
